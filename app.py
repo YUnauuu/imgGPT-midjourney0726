d105e5a8c5adfb0084bef29236edd7c14bcfe83d
YUnauuu <101491084+YUnauuu@users.noreply.github.com>
2023-07-26 16:22:58 +0800

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,8 @@
 from flask import Flask,render_template,request
 import replicate
 import os
+import json
+import time
 
 
 os.environ["REPLICATE_API_TOKEN"] = "r8_dE4v9NTlKMOK35GKuFtMLhu7UiKY8AT3aLdPV"
@@ -19,8 +21,16 @@ app = Flask(__name__)
 def index():
     if request.method == "POST":
         q = request.form.get("question")
-        i = {"prompt": q}
-        r = version.predict(**i)
+        
+        body = json.dumps({"version": "db21e45d3f7023abc2a46ee38a23973f6dce16bb082a930b0c49861f96d1e5bf", "input": { "prompt": q } })
+        headers = {'Authorization': 'Token r8_dE4v9NTlKMOK35GKuFtMLhu7UiKY8AT3aLdPV','Content-Type': 'application/json'}
+        output = requests.post('https://api.replicate.com/v1/predictions',data=body,headers=headers)
+        time.sleep(10)
+        get_url = output.json()['urls']['get']
+        
+        r = requests.post(get_url,headers=headers).json()['output']
+
+        
         return(render_template("index.html",result=r[0]))
     else:
         return(render_template("index.html",result="waiting"))
