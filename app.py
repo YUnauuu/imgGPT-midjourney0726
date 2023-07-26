afbae5906e6b4defdd076d0d001f5714249d2456
YUnauuu <101491084+YUnauuu@users.noreply.github.com>
2023-07-26 16:31:03 +0800

    Update app.py

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ def index():
         q = request.form.get("question")
         
         body = json.dumps({"version": "db21e45d3f7023abc2a46ee38a23973f6dce16bb082a930b0c49861f96d1e5bf", "input": { "prompt": q } })
-        headers = {'Authorization': 'Token 787f515cb0624813736c11e7fefec66473394f02','Content-Type': 'application/json'}
+        headers = {'Authorization': 'Token r8_dE4v9NTlKMOK35GKuFtMLhu7UiKY8AT3aLdPV','Content-Type': 'application/json'}
         output = requests.post('https://api.replicate.com/v1/predictions',data=body,headers=headers)
         time.sleep(10)
         get_url = output.json()['urls']['get']
